8f2047106d747e3a507c7152ce2eac60ac94fe9e
erioluwadivine <divineayomi@gmail.com>
2018-07-28 09:11:36 -0400

    add table

--- api.py
+++ api.py
@@ -9,7 +9,16 @@ import random
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+class User(db.Model):
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(32), primary_key=True)
+    fullname = db.Column(db.String(32), index=True)
+    username = db.Column(db.String(32), index=True)
+    phone_no = db.Column(db.String(32), primary_key=True)
+    password = db.Column(db.String(64))
 
+db.create_all()
 
 
 if __name__=="__main__":
